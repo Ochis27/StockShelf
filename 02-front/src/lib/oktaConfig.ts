01961515b786590a4ec16dee83cb08aebe6ce952
Ochis27 <74562957+Ochis27@users.noreply.github.com>
2024-06-27 17:44:46 +0300

    Delete 02-front directory

--- 02-front/src/lib/oktaConfig.ts
+++
@@ -1,8 +0,0 @@
-export const oktaConfig = {
-  clientId: "0oah2wdw7nOnnx5mj5d7",
-  issuer: "https://dev-70032746.okta.com/oauth2/default",
-  redirectUri: "https://localhost:3000/login/callback",
-  scopes: ["openid", "profile", "email"],
-  pkce: true,
-  disableHttpsCheck: true,
-};
